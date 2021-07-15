a5a0e6d769cf26e69b0d7cee838e15e81cfcb8ae
Matt Sinclair <msin553@aucklanduni.ac.nz>
2021-07-15 17:10:50 +1200

    feat: display card sum

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
 let sumEl = document.getElementById("sum-el")
-console.log(sumEl)
+let cardsEl = document.getElementById("cards-el")
+
 function startGame() {
     sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
