ebdf2d6c01760ab90107eb9a04c8c9d9092dadae
Matt Sinclair <msin553@aucklanduni.ac.nz>
2021-07-19 22:01:54 +1200

    Refactor: Formatting with Prettier for VS code

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-function startGame(){
-    renderGame()
+function startGame() {
+  renderGame();
 }
 
-function getRandomCard(){
-    return (Math.floor(Math.random() * 13)) + 1
+function getRandomCard() {
+  let num = Math.floor(Math.random() * 13) + 1;
 }
 
 function renderGame() {
-    sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++){
-        card = cards[i]
-        cardsEl.textContent += card + " "
-    }
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message    
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    card = cards[i];
+    cardsEl.textContent += card + " ";
+  }
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
 
-function newCard(){
-    console.log("New Card")
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+function newCard() {
+  console.log("New Card");
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+  console.log(cards);
+  renderGame();
 }
-
