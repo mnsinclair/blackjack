4581094403e3e5f74ab90c54d3a40073633fe34a
Matt Sinclair <msin553@aucklanduni.ac.nz>
2021-07-19 22:17:33 +1200

    Refactor

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
-  firstCard = getRandomCard();
-  secondCard = getRandomCard();
-  cards = [firstCard, secondCard];
-  sum = firstCard + secondCard;
+  cards = [getRandomCard(), getRandomCard()];
+  sum = cards[0] + cards[1];
   hasBlackJack = false;
   isAlive = true;
   message = "";
